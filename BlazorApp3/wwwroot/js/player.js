d5622129fddfa24e8c42958d01bed8193ba8fa9f
rupisaini123 <47219093+rupisaini123@users.noreply.github.com>
2021-01-28 11:05:16 -0700

    video js

--- BlazorApp3/wwwroot/js/player.js
+++ BlazorApp3/wwwroot/js/player.js
@@ -1,3 +1,19 @@
 ﻿function loadPlayer(id, options) {
-    videojs(id, options);
+    var player = videojs(id, options);
+    var titleOptions = {
+        title: 'Akbar ka bal Birbal',
+        floatPosition: 'left', //Float left or right (to prevent big play button overlap) (default left)
+            margin: '10px', //Margin from top/left/right (default 10px)
+                fontSize: '2em', //font size (default 1em)
+                    debug: false //true or false. Will output debug messages for title status
+    //logger: LOGOBJECT //must have a function called log that it can call to, otherwise will use a default basic log func
+            };
+    player.titleoverlay(titleOptions);
+     player.seekButtons({
+         forward: 10,
+         back: 10
+     });
+
+     player.mobileUi();
+
 }
