9ea5ab7408a42b595898c36b29f565dd0c4e56f3
rupisaini123 <47219093+rupisaini123@users.noreply.github.com>
2021-01-28 09:29:11 -0700

    Test 2

---
+++ BlazorApp3.Shared/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BlazorApp3.Shared
+{
+    public class User
+    {
+        public string Username { get; set; } = "Rupinder";
+    }
+}
---
+++ BlazorApp3/Controls/InputValidationBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp3.Controls
+{
+    public class InputValidationBase : ComponentBase
+    {
+        [Parameter]
+        public string Value { get; set; }
+
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+        protected async void inputChange(ChangeEventArgs eventArgs)
+        {
+            await ValueChanged.InvokeAsync(eventArgs.Value.ToString());
+        }
+    }
+}
